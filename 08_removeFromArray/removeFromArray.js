54f18a57eb4912450495d17d6314efb52e0358df
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-08-17 10:20:52 -0700

    fix: complete exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+    if (arguments.length < 2){
+        throw new Error("Please provide the array and at least one item to be removed.");
+    }
+    let arr = arguments[0];
+    for (let i = 1; i < arguments.length; i++){
+        const num = arguments[i];
+        while (arr.includes(num)){
+            arr.splice(arr.indexOf(num), 1);
+        }
+        
+    }
+    
+    return arr;
 };
 
 // Do not edit below this line
