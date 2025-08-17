f0b88a3029f71e245918b41b923490e8a7514f7e
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-08-17 09:36:32 -0700

    fix: complete exercise 08

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(arr) {
+    let ret = [];
+    for (let i = arr.length - 1; i >= 0; i--){
+        ret.push(arr.charAt(i));
+    }
+    ret = ret.join("");
 
+    return ret;
 };
 
 // Do not edit below this line
