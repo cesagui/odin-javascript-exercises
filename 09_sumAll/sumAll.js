fb6a2947ff605de0572a454f8a764c278c6d7ca2
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-08-17 10:29:32 -0700

    fix: complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let total = 0;
+    if (typeof(start) != "number" || typeof(end) != "number"){
+        return "ERROR";
+    }
+    if(start != Math.floor(start) || end != Math.floor(end)){
+        return "ERROR";
+    }
+    if (end < 0 || start < 0){
+        return "ERROR";
+    }
+    if (start > end){
+        // swap the vals
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+    while(start <= end){
+        total += start;
+        start++;
+    }
+    return total
 };
 
 // Do not edit below this line
