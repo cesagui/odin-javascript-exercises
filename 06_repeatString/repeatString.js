d7dc9e54c5eb6f1152e5da3ab79b79e4923ecb9e
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-08-17 09:29:12 -0700

    fix: complete exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    let ret = "";
+    if (count < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i < count; i++){
+        ret += str;
+    }
+    return ret;
 };
 
 // Do not edit below this line
