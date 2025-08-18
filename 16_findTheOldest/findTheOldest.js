f4e7839e9812159aad0247c979fe24e1a4e8b0f4
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-08-17 20:17:17 -0700

    fix: complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currentPerson) =>{
+        if (!('yearOfDeath' in currentPerson)){
+            currentPerson.yearOfDeath = new Date().getFullYear();
+        }
+        if ((currentPerson.yearOfDeath - currentPerson.yearOfBirth) > (oldestPerson.yearOfDeath - oldestPerson.yearOfBirth)){
+            return currentPerson;
+        }
+        return oldestPerson;
+    }, arr[0]);
 };
 
 // Do not edit below this line
