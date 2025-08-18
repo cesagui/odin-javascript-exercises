e369da3e5225936c705f450c930c8d7613b5ec87
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-08-17 20:03:47 -0700

    fix: complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    // base case
+    num = Number(num);
+    if (num < 0 || Math.floor(num) != num){
+        return "OOPS";
+    }
+    if (num === 0){
+        return 0;
+    }
+    if (num === 1){
+        return 1;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
