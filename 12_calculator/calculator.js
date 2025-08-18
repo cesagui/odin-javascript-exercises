fea38a33697c2b248964d5bbd1e7143a49c82761
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-08-17 19:43:29 -0700

    fix: complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, number) => {
+    total += number;
+    return total;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, str) => {
+    product *= Number(str);
+    return product;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let ret = 1;
+  let i = 0;
+  while (i < a){
+    i++;
+    ret *= i;
+  }
+  return ret;
 };
 
 // Do not edit below this line
